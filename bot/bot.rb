e32b2f4ce7b68ea748dd61bc54ed06a26768bfc9
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-07-18 20:04:17 +0800

    code review command

--- bot/bot.rb
+++ bot/bot.rb
@@ -1,6 +1,28 @@
 class Bot < SlackRubyBot::Bot
   @id = 0
 
+  SENIOR_DEV_IDS = [
+    "U4SK3RBPS",
+    "U5AG3CY7M"
+  ];
+
+  DEV_IDS = [
+    "U5RT71ZKQ",
+    "U0YL17DBQ",
+    "U4TDX4QSH",
+    "U4X6FDG95",
+    "U5VFECCPM",
+  ];
+
+  # @dc = "U5RT71ZKQ"
+  # @earle = "U0YL17DBQ"
+  # @francis = "U4TDX4QSH"
+  # @nmfdelacruz = "U4SK3RBPS"
+  # @sachink = "U5X3KN2AV"
+  # @vicente = "U5AG3CY7M"
+  # @yogesh.khather = "U4X6FDG95"
+  # @rickdtrick = "U5VFECCPM"
+
   def self.next_id
     @id = @id % 10 + 1
   end
@@ -12,5 +34,26 @@ class Bot < SlackRubyBot::Bot
 
   command 'who is the handsomest guy in tech?' do |client, data, match|
     client.say(channel: data.channel, text: "It's Brian 'the Dragon' McKiernan!! Chat him at <@U2DHPSA9J>")
+  end
+
+  command 'reviewer for' do |client, data, match|
+    senior_dev_ids = SENIOR_DEV_IDS - [data.user]
+    dev_ids = DEV_IDS - [data.user]
+
+    senior_dev_index = Random.rand(0...senior_dev_ids.size)
+    dev_index = Random.rand(0...dev_ids.size)
+
+    senior_dev_id = senior_dev_ids[senior_dev_index]
+    dev_id = dev_ids[dev_index]
+
+    pr_no = match['expression']
+
+    message = "Hi <@#{dev_id}> and <@#{senior_dev_id}>! " +
+      "<@#{data.user}> asked you to review this PR here https://github.com/carabao-capital/first-circle-app/pull/#{pr_no}"
+
+    client.say({
+      channel: data.channel,
+      text: message,
+    })
   end
 end
