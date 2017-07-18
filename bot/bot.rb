bdb1778a34f9c5c56e3bfabcf49a11de1b9f1553
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-07-18 19:54:11 +0800

    Add Brian's command

--- bot/bot.rb
+++ bot/bot.rb
@@ -9,4 +9,8 @@ class Bot < SlackRubyBot::Bot
     Rails.cache.write next_id, { text: match['expression'] }
     client.say(channel: data.channel, text: match['expression'])
   end
+
+  command 'who is the handsomest guy in tech?' do |client, data, match|
+    client.say(channel: data.channel, text: "It's Brian 'the Dragon' McKiernan!! Chat him at <@U2DHPSA9J>")
+  end
 end
