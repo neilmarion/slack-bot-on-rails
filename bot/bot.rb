78c4ed3274b25f599432ee1deed69917a2d935f3
Neil Marion dela Cruz <641821+neilmarion@users.noreply.github.com>
2018-04-14 22:43:08 +0800

    Update

--- bot/bot.rb
+++ bot/bot.rb
@@ -1,57 +1,154 @@
 class Bot < SlackRubyBot::Bot
-  @id = 0
+  # Getting user slack user ids https://slack.com/api/users.list?token=xoxp-5132160299-162649861808-214565164807-14d6f103c6022ed767e25f26a027ffaa&pretty=1
 
-  SENIOR_DEV_IDS = [
-    "U4SK3RBPS",
-    "U5AG3CY7M",
-    "U6N5LSTJR",
-  ];
+  DEV_IDS = {
+    rgabriel: "U6JUFRGUU",
+    ptorre: "U890X3XL2",
+    jessc: "U6N5LSTJR",
+    dc: "U5RT71ZKQ",
+    earle: "U0YL17DBQ",
+    francis: "U4TDX4QSH",
+    nmfdelacruz: "U4SK3RBPS",
+    rickdtrick: "U5VFECCPM",
+    angelique: "U4DTJAPU3",
+  }
 
-  DEV_IDS = [
-    "U5RT71ZKQ",
-    "U0YL17DBQ",
-    "U4TDX4QSH",
-    "U5VFECCPM",
-    "U6JUFRGUU",
-  ];
+  TECH_OPS = [:rgabriel, :jessc, :earle, :francis, :angelique]
+  FCA_V2 = [:rgabriel, :earle, :francis]
+  MOBILE = [:ptorre, :dc, :nmfdelacruz]
+  COMMERCIAL = [:nmfdelacruz, :ptorre, :dc, :rickdtrick]
+  FRONTEND = [:rickdtrick, :francis]
+  ALL = [
+    :rgabriel,
+    :ptorre,
+    :jessc,
+    :dc,
+    :earle,
+    :francis,
+    :nmfdelacruz,
+    :rickdtrick,
+    :angelique,
+  ]
 
-  # @dc = "U5RT71ZKQ"
-  # @earle = "U0YL17DBQ"
-  # @francis = "U4TDX4QSH"
-  # @nmfdelacruz = "U4SK3RBPS"
-  # @sachink = "U5X3KN2AV"
-  # @vicente = "U5AG3CY7M"
-  # @yogesh.khather = "U4X6FDG95"
-  # @rickdtrick = "U5VFECCPM"
+  command '--help' do |client, data, match|
+    string = "```
+plankbot, version 0.3, https://github.com/neilmarion/slack-bot-on-rails
 
-  def self.next_id
-    @id = @id % 10 + 1
+usage: @plankbot review -R <repo name> -P <PR number> [-T <team>] [-X member/s]
+Chooses at most two code reviewers for a PR
+
+Options:
+  -R    repository name
+  -P    pull request number
+  -T    team name
+  -X    ommit member, comma separated and no spaces between
+
+teams:
+  tech_ops (rgabriel, jessc, earle, francis, angelique)
+  fca_v2 (rgabriel, earle, francis)
+  mobile (ptorre, dc, nmfdelacruz)
+  commercial (nmfdelacruz, ptorre, dc, rickdtrick)
+  frontend (rickdtrick, francis)
+
+members:
+  angelique, dc, earle, francis, jessc, nmfdelacruz, ptorre, rgabriel, rickdtrick
+
+examples:
+  1. @plankbot review -R first-circle-app -P 6672 -X nmfdelacruz,rickdtrick
+  2. @plankbot review -R fca_mobile_react -P 102 -T mobile
+  3. @plankbot review -R first-circle-account -P 13 -T fca_v2 -X francis
+
+PROCESS SUMMARY:
+      make branch
+    +-> PR (WIP label)
+    |     writing code
+    |       writing tests
+    |         PR (REVIEW label)
+    +---------- QA and code review
+                  branch merge to master
+                    production smoke testing
+
+wake up:
+  Go to http://plankbotfc.herokuapp.com/
+```"
+
+    client.say(channel: data.channel, text: string)
   end
 
-  command 'say' do |client, data, match|
-    Rails.cache.write next_id, { text: match['expression'] }
-    client.say(channel: data.channel, text: match['expression'])
+  def self.build_url(options)
+    "https://github.com/carabao-capital/#{options[:repo]}/pull/#{options[:pr_no]}"
   end
 
-  command 'who is the handsomest guy in tech?' do |client, data, match|
-    client.say(channel: data.channel, text: "It's Brian 'the Dragon' McKiernan!! Chat him at <@U2DHPSA9J>")
+  def self.team(team)
+    case team
+    when "tech_ops"
+      TECH_OPS
+    when "fca_v2"
+      FCA_V2
+    when "mobile"
+      MOBILE
+    when "commercial"
+      COMMERCIAL
+    when "frontend"
+      FRONTEND
+    else
+      ALL
+    end
   end
 
-  command 'reviewer for' do |client, data, match|
-    senior_dev_ids = SENIOR_DEV_IDS - [data.user]
-    dev_ids = DEV_IDS - [data.user]
+  def self.randomize(team, members_to_ommit, user_id)
+    remaining_members = team - members_to_ommit.map(&:to_sym)
 
-    senior_dev_index = Random.rand(0...senior_dev_ids.size)
-    dev_index = Random.rand(0...dev_ids.size)
+    dev_ids = remaining_members.map{|m| DEV_IDS[m] }
+    dev_ids = dev_ids - [user_id]
+    chosen_dev_ids = []
 
-    senior_dev_id = senior_dev_ids[senior_dev_index]
-    dev_id = dev_ids[dev_index]
+    if dev_ids.size <= 2
+      return dev_ids
+    else
+      first_chosen = dev_ids[Random.rand(0...dev_ids.size)]
+      chosen_dev_ids << first_chosen
 
-    pr_no = match['expression']
+      loop do
+        second_chosen = dev_ids[Random.rand(0...dev_ids.size)]
+        if second_chosen != first_chosen
+          chosen_dev_ids << second_chosen
+          break
+        end
+      end
+    end
 
-    message = "Hi <@#{dev_id}> and <@#{senior_dev_id}>! " +
-      "<@#{data.user}> asked you to review this PR here https://github.com/carabao-capital/first-circle-app/pull/#{pr_no}"
+    chosen_dev_ids
+  end
 
+  command 'review' do |client, data, match|
+    string = match['expression']
+    string_array = string.split(' ')
+
+    url_options = {}
+    chosen_team = ALL
+    members_to_ommit = []
+
+    string_array.each_with_index do |e, counter|
+      next if counter % 2 == 1
+
+      case e
+      when "-R"
+        url_options[:repo] = string_array[counter + 1]
+      when "-P"
+        url_options[:pr_no] = string_array[counter + 1]
+      when "-T"
+        chosen_team = team(string_array[counter + 1])
+      when "-X"
+        members_to_ommit = string_array[counter + 1].split(',')
+      end
+    end
+
+    chosen_reviewer_ids = randomize(chosen_team, members_to_ommit, data.user)
+
+    chosen_reviewer_ids.map{|e| "<@#{e}>"}
+
+    message = "Hi #{chosen_reviewer_ids.map{|e| "<@#{e}>"}.join(" and ")}! <@#{data.user}> asked to review #{build_url(url_options)}"
     client.say({
       channel: data.channel,
       text: message,
